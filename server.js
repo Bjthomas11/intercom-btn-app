ea2e75b7da88d08af4235ac4b120b78c3a5c3e4b
Glitch (plucky-krill) <none>
2019-07-01 16:54:46 +0000

    🍶🗽 Checkpoint ./server.js:2050926/4180

--- server.js
+++ server.js
@@ -1,102 +1,55 @@
-
 /* INSERT CODE HERE */
-"use strict";
 const express = require('express');
 const bodyParser = require('body-parser');
-var Intercom = require('intercom-client');
-// const jsdom = require("jsdom");
-// const {JSDOM} = jsdom;
-
 const app = express();
-// app.use(bodyParser);
 
-// require("jsdom").env("", function(err, window) {
-//     if (err) {
-//         console.error(err);
-//         return;
-//     }
- 
-//     var $ = require("jquery")(window);
-// });
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
 
 // http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-// app.use(express.static('public'));
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
         
 const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-
 /* 
   This is an endpoint that Intercom will POST HTTP request when the card needs to be initialized.
   This can happen when your teammate inserts the app into the inbox, or a new conversation is viewed.
-  
-  
 */
 
-// Lead api https://api.intercom.io/contacts
-
-// LIST ALL LEADS
-// $ curl https://api.intercom.io/contacts \
-// -H 'Authorization:Bearer G9rOjIyM2JkMzA2XzBhNGZfNDkwOF9hYmEzXzg4NDM0ZDljNjM5MjoxOjA='' \
-// -H 'Accept: application/json'
-
-// token: dG9rOjIyM2JkMzA2XzBhNGZfNDkwOF9hYmEzXzg4NDM0ZDljNjM5MjoxOjA
-// -H 'Authorization:Bearer dG9rOjIyM2JkMzA2XzBhNGZfNDkwOF9hYmEzXzg4NDM0ZDljNjM5MjoxOjA=' \
-
-// const requiredFields = ["firstName", "lastName", "address",  "phoneNumber", "address", "city", "state", "zip"];
-//   for (let i = 0; i < requiredFields.length; i++){
-//     const field = requiredFields[i];
-//     if(!(field in request.body)){
-//       const message = `Missing ${field} field`;
-//       console.error(message);
-//       return response.status(400).send(message);
-//     }
-//   }
-
-app.post("/initialize", (request, response) => {
-  const body = request.body;
+app.post("/initialize", (request, response) => {  
+  const body = request.body;  
   response.send({
     canvas: {
       content: {
         components: [
-          { type: "button", label: "Not functioning yet", style: "primary", id: "lead-button", action: {type: "submit"} },
+          { type: "button", label: "Click Me!", style: "primary", id: "url_button", action: {type: "submit"} },
         ], 
       },
     },
   });
 });
 
-
-app.post("/submit", (request, response) => { 
-  const body = request.body;
+app.post("/submit", (request, response) => {  
+  // console.log(request);
+  // console.log(response);
+  const body = request.body;  
   console.log(body);
   response.send({
     canvas: {
       content: {
         components: [
-          { type: "text", text: "test"
-          },
+          { type: "text", text: "test", 
+           style: "header", align: "center" },
         ], 
       },
     },
   });
 });
-
-app.post("/submit", (request,response) => {
-  console.log(request);
-});
-
-app.post("/submit", (request,response) => {
-  console.log(response);
-})
-
-
-// https://glossy-century-1.glitch.me/initialize
-// https://glossy-century-1.glitch.me/submit
