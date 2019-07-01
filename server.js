8764c07b709d2ffeda857c2b16836ed1697f0532
Glitch (plucky-krill) <none>
2019-07-01 18:31:47 +0000

    🦂🙈 Checkpoint ./server.js:2050926/3526

--- server.js
+++ server.js
@@ -57,6 +57,48 @@ app.post("/submit", (request, response) => {
   var state = body.customer.custom_attributes["Property State or Province"];
   var zip = body.customer.custom_attributes["Property Postal Code"];
   if(name && phone && street_address && city && state && zip){
+      var lead = new Object();
+      lead.APIKey = "1234234";
+      lead.AppID = "12";
+      lead.name = body.customer.name.val();
+      lead.phone = body.customer.phone.val();
+      lead.email = body.customer.email.val();
+      lead.street_address = body.customer.custom_attributes["Property Street Address"].val();
+      lead.city = body.customer.custom_attributes["Property City"].val();
+      lead.state = body.customer.custom_attributes["Property State or Province"].val();
+      lead.zip = body.customer.custom_attributes["Property Postal Code"].val();
+      lead.leadSource = "test";
+      lead.custom1 = $("#custom1").val();
+      lead.id = "1";
+
+      var APIKey = "1234234";
+      var RefID = "12";
+
+      function make_base_auth(APIKey, RefID) {
+        var tok = btoa(APIKey) + ":" + btoa(RefID);
+
+        return "Basic " + tok;
+      }
+
+      $.ajax({
+        url: "http://arch-form.advestors.net/v2/leads/create",
+        type: "POST",
+        contentType: "application/json",
+
+        data: JSON.stringify(lead),
+        beforeSend: function(data) {
+          data.setRequestHeader(
+            "Authorization",
+            make_base_auth(APIKey, RefID)
+          );
+        },
+        success: function(data, textStatus, xhr) {
+          console.log(JSON.stringify(data));
+        },
+        error: function(xhr, textStatus, errorThrown) {
+          console.log("Error");
+        }
+      });
     console.log("test");
     response.send({
       canvas: {
@@ -69,6 +111,7 @@ app.post("/submit", (request, response) => {
       },
     });
   } else {
+    console.log("empty");
     response.send({
     canvas: {
       content: {
